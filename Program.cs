0b3836d31f8b1d09f15d45e1661098a4682a94e1
Vincent Xu <vincent.xu1@outlook.com>
2025-07-08 13:45:18 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment_1._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers: ");
+
+            // 1.1.2
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Sum: {a + b}");
+
+            // 1.1.3
+            Console.WriteLine($"Quotient: {a / b}");
+            Console.WriteLine($"Remainder: {a % b}");
+        }
+    }
+}
